bedcb8b6f284fbee8dc3e610bcfee2be71363b60
anwar <anouar.ab95@gmail.com>
2024-02-21 21:20:35 +0100

    update

--- database/migrations/2024_02_21_166666_create_cheises_table.php
+++ database/migrations/2024_02_21_166666_create_cheises_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        //ch
+        //cheise
         Schema::create('cheises', function (Blueprint $table) {
             $table->id();
             $table->boolean('is_booked');
