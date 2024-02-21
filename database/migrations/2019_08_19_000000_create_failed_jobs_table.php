4e69d59659ffc62b4d93309a451ad2321873468d
anouar <anouar.ab95@gmail.com>
2024-02-21 17:06:35 +0100

    create datatbase

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -4,13 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
+        //commit
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
             $table->string('uuid')->unique();
